c01f29af8ef51a88822bfadbfe50ebfe70e9a580
thibackend <thi.a24technology@gmail.com>
2023-04-11 07:37:05 +0700

    done lession 1 and lession 2.

---
+++ lession_1/index.js
@@ -0,0 +1,24 @@
+// declare _____________________;
+
+// var fullName = "A Thi";
+// alert(" HI Thi");
+// var age = 14;
+// alert(age);
+
+
+
+
+
+// comment ___________________________;
+
+
+
+/**
+ hello this is block comment
+ */
+
+ // this is comment row by row
+
+ /*
+ phim tat: "ctrl +/" in 
+ */
---
+++ lession_2/index.js
@@ -0,0 +1,33 @@
+/* 
+learn some buit-in function
+
+1 .alert();
+2. console...().
+3. confirm();
+4.prompt
+4.set timeout
+5.set interval
+
+
+*/
+
+
+
+// Alert.
+
+
+//alert("alert use to notify and have a message out the browser");
+
+
+
+//Console 
+/*
+
+
+
+*/
+
+console.log("___console.log used to show sth out console");
+console.warn("___console.warn used to show sth out with color yellow like warning");
+console.error("__to show sth err and have a red message");
+
